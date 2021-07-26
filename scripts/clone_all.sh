4fb830d52e2cd99ad0f0632c8844ec49865abc01
richelbilderbeek <richel@richelbilderbeek.nl>
2021-07-26 10:12:49 +0200

    Add 'cloe_all'

---
+++ scripts/clone_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Clone all my Singularity examples
+#
+# Usage:
+#
+# ./singularity_examples/scripts/close_all.sh
+#
+# to create folders 'singularity_example_[x]' in the working directory.
+
+git clone https://github.com/richelbilderbeek/singularity_example_1
+git clone https://github.com/richelbilderbeek/singularity_example_2
+git clone https://github.com/richelbilderbeek/singularity_example_3
+git clone https://github.com/richelbilderbeek/singularity_example_4
+git clone https://github.com/richelbilderbeek/singularity_example_5
+git clone https://github.com/richelbilderbeek/singularity_example_6
+git clone https://github.com/richelbilderbeek/singularity_example_7
+git clone https://github.com/richelbilderbeek/singularity_example_8
+git clone https://github.com/richelbilderbeek/singularity_example_9
+git clone https://github.com/richelbilderbeek/singularity_example_10
+git clone https://github.com/richelbilderbeek/singularity_example_11
+git clone https://github.com/richelbilderbeek/singularity_example_12
+git clone https://github.com/richelbilderbeek/singularity_example_13
+git clone https://github.com/richelbilderbeek/singularity_example_14
+git clone https://github.com/richelbilderbeek/singularity_example_15
+git clone https://github.com/richelbilderbeek/singularity_example_16
+git clone https://github.com/richelbilderbeek/singularity_example_17
+git clone https://github.com/richelbilderbeek/singularity_example_18
+
